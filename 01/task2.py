4207df90aa443fc022c56df4866ed6d1211160ed
Daniel Sparber <daniel@sparber.io>
2021-12-05 13:11:38 +0100

    Cleaner solution for 01/task2

--- 01/task2.py
+++ 01/task2.py
@@ -1,20 +1,10 @@
 
 with open("input", "r") as f:
-    lines = f.readlines()
-    numbers = [int(v) for v in lines]
+    numbers = [int(v) for v in f.readlines()]
 
-    count = -1
-    last_sum = -1
-    for i in range(len(numbers) - 2):
-        sliding_sum = 0
-        for j in range(3):
-            sliding_sum += numbers[i + j]
-
-        print(sliding_sum)
-
-        if sliding_sum > last_sum:
+    count = 0
+    for i in range(1, len(numbers) - 2):
+        if sum(numbers[i:(i+3)]) > sum(numbers[(i-1):(i+2)]):
             count += 1
 
-        last_sum = sliding_sum
-
-    print("Result: {}".format(count))
+    print(count)
