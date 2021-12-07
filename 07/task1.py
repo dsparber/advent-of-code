8876d87654d69be1657c539c34836dacf7b059f1
Daniel Sparber <daniel@sparber.io>
2021-12-07 08:21:35 +0100

    Implemented day 07

---
+++ 07/task1.py
@@ -0,0 +1,12 @@
+with open("input", "r") as f:
+    lines = [line.strip() for line in f.readlines()]
+    positions = [int(x) for x in lines[0].split(",")]
+
+    min_pos = min(positions)
+    max_pos = max(positions)
+
+    min_fuel = len(positions) * (max_pos - min_pos)  # An upper bound
+    for p in range(min_pos, max_pos + 1):
+        min_fuel = min(min_fuel, sum([abs(pos - p) for pos in positions]))
+
+    print(min_fuel)
