36e2e79a09ee8b91e763e168f6c1bfa663be0cc1
Daniel Sparber <daniel@sparber.io>
2022-12-16 17:04:31 +0100

    Day 16 - deleted half the code

--- 2022/16/solution.py
+++ 2022/16/solution.py
@@ -7,40 +7,18 @@ valves = dict()
 
 
 @cache
-def max_pressure_release(open_valves: tuple[str], current: str, time: int) -> int:
+def max_pressure_release(open_valves: tuple[str], current: str, time: int, with_elephant: bool) -> int:
     if time <= 0:
-        return 0
+        return max_pressure_release(open_valves, "AA", 26, False) if with_elephant else 0
 
     best_score = 0
-    if current not in open_valves:
-        flow = valves[current]['flow']
-        if flow > 0:
-            open_valves = tuple(sorted([current, *open_valves]))
-            pressure_release = (time - 1) * flow
-            best_score = pressure_release + max_pressure_release(open_valves, current, time - 1)
+    if current not in open_valves and valves[current]['flow'] > 0:
+        open_valves = tuple(sorted([current, *open_valves]))
+        pressure_release = (time - 1) * valves[current]['flow']
+        best_score = pressure_release + max_pressure_release(open_valves, current, time - 1, with_elephant)
 
     for neighbor in valves[current]['tunnels']:
-        best_score = max(best_score, max_pressure_release(open_valves, neighbor, time - 1))
-
-    return best_score
-
-
-@cache
-def max_pressure_release_with_elephant(open_valves: tuple[str], current: str, time: int) -> int:
-    if time <= 0:
-        # Elephant is starting now
-        return max_pressure_release(open_valves, "AA", 26)
-
-    best_score = 0
-    if current not in open_valves:
-        flow = valves[current]['flow']
-        if flow > 0:
-            open_valves = tuple(sorted([current, *open_valves]))
-            pressure_release = (time - 1) * flow
-            best_score = pressure_release + max_pressure_release_with_elephant(open_valves, current, time - 1)
-
-    for neighbor in valves[current]['tunnels']:
-        best_score = max(best_score, max_pressure_release_with_elephant(open_valves, neighbor, time - 1))
+        best_score = max(best_score, max_pressure_release(open_valves, neighbor, time - 1, with_elephant))
 
     return best_score
 
@@ -55,11 +33,8 @@ def solve(input_data: str) -> Iterable[int]:
         )
 
     max_pressure_release.cache_clear()
-    yield max_pressure_release(tuple(), 'AA', 30)
-
-    max_pressure_release.cache_clear()
-    max_pressure_release_with_elephant.cache_clear()
-    yield max_pressure_release_with_elephant(tuple(), 'AA', 26)
+    yield max_pressure_release(tuple(), 'AA', 30, False)
+    yield max_pressure_release(tuple(), 'AA', 26, True)
 
 
 run(solve)
