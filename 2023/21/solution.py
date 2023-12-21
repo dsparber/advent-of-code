a1fa0c7fd935f1d915136186e9b26455cc7a2403
Daniel Sparber <daniel@sparber.io>
2023-12-21 11:17:48 +0100

    Solved day 21, part 2

--- 2023/21/solution.py
+++ 2023/21/solution.py
@@ -5,19 +5,38 @@ from utils.grid import Grid
 
 
 def solve(input_data: str) -> Iterable[int]:
-    grid = Grid.parse_char_matrix(input_data)
+    grid = Grid.parse_char_matrix(input_data, repeats_infinitely=True)
 
-    positions = {grid.find("S")}
-    target_steps = 6 if grid.rows == 11 else 64
-    for _ in range(target_steps):
-        positions = {
+    is_sample = grid.rows == 11
+    yield num_reachable(grid, 6 if is_sample else 64)
+    yield num_reachable(grid, 26_501_365)
+
+
+def num_reachable(grid: Grid, steps: int) -> int:
+    start = grid.find("S")
+    visited = {}
+    new_positions = {start}
+    num_positions = {0: 1}
+
+    for step in range(1, grid.rows * 3):
+        visited, new_positions = new_positions, {
             neighbor
-            for position in positions
-            for neighbor in grid.neighbors(position)
-            if grid[neighbor] != "#"
+            for p in new_positions
+            for neighbor in grid.neighbors(p)
+            if neighbor not in visited and grid[neighbor] != "#"
         }
+        num_positions[step] = len(new_positions) + num_positions.get(step - 2, 0)
 
-    yield len(positions)
+    # Decompose: steps = kn + r
+    n = grid.rows
+    k, r = steps // n, steps % n
+
+    positions_1n_r = num_positions[n + r]
+    positions_2n_r = num_positions[2 * n + r]
+    d1 = positions_2n_r - positions_1n_r
+    d2 = positions_2n_r + num_positions[r] - 2 * positions_1n_r
+
+    return positions_2n_r + (k - 2) * (d1 + (k - 1) * d2 // 2)
 
 
 run(solve)
--- utils/grid.py
+++ utils/grid.py
@@ -4,12 +4,22 @@ from . import directions
 
 
 class Grid[T]:
-    def __init__(self, values: dict[tuple[int, int], T], rows: int, cols: int):
+    def __init__(
+        self,
+        values: dict[tuple[int, int], T],
+        rows: int,
+        cols: int,
+        repeats_infinitely: bool = False,
+    ):
         self.values: dict[tuple[int, int], T] = values
         self.rows: int = rows
         self.cols: int = cols
+        self.repeats_infinitely = repeats_infinitely
 
     def __getitem__(self, key: tuple[int, int]) -> T:
+        if self.repeats_infinitely:
+            i, j = key
+            return self.values.get((i % self.rows, j % self.cols))
         return self.values.get(key)
 
     @property
@@ -17,6 +27,9 @@ class Grid[T]:
         return self.rows - 1, self.cols - 1
 
     def is_within_bounds(self, index: tuple[int, int]) -> bool:
+        if self.repeats_infinitely:
+            return True
+
         i, j = index
         return 0 <= i < self.rows and 0 <= j < self.cols
 
@@ -37,17 +50,25 @@ class Grid[T]:
         return f"Grid({self.rows}, {self.cols})"
 
     @staticmethod
-    def parse_char_matrix(input_data: str) -> "Grid[str]":
-        return Grid.parse_grid_with_value_mapper(input_data, str)
+    def parse_char_matrix(
+        input_data: str, repeats_infinitely: bool = False
+    ) -> "Grid[str]":
+        return Grid.parse_grid_with_value_mapper(input_data, str, repeats_infinitely)
 
     @staticmethod
-    def parse_digit_matrix(input_data: str) -> "Grid[int]":
-        return Grid.parse_grid_with_value_mapper(input_data, int)
+    def parse_digit_matrix(
+        input_data: str, repeats_infinitely: bool = False
+    ) -> "Grid[int]":
+        return Grid.parse_grid_with_value_mapper(input_data, int, repeats_infinitely)
 
     @staticmethod
     def parse_grid_with_value_mapper[
         T
-    ](input_data: str, value_mapper: Callable[[str], T]) -> "Grid[T]":
+    ](
+        input_data: str,
+        value_mapper: Callable[[str], T],
+        repeats_infinitely: bool = False,
+    ) -> "Grid[T]":
         values = {
             (i, j): value_mapper(value)
             for i, line in enumerate(input_data.splitlines())
@@ -56,4 +77,4 @@ class Grid[T]:
         rows = max([i for i, _ in values.keys()]) + 1
         cols = max([j for _, j in values.keys()]) + 1
 
-        return Grid(values, rows, cols)
+        return Grid(values, rows, cols, repeats_infinitely)
